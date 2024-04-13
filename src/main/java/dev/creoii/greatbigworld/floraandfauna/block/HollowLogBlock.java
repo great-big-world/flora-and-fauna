2d4fe3f16bcc9064b5527468070a4ce516001814
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-04-12 20:42:37 -0400

    Implement Creo API 0.4.2

--- src/main/java/dev/creoii/greatbigworld/floraandfauna/block/HollowLogBlock.java
+++ src/main/java/dev/creoii/greatbigworld/floraandfauna/block/HollowLogBlock.java
@@ -1,6 +1,9 @@
 package dev.creoii.greatbigworld.floraandfauna.block;
 
+import dev.creoii.creoapi.api.block.CreoBlock;
 import net.minecraft.block.*;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityPose;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
@@ -9,11 +12,12 @@ import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3i;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 
-public class HollowLogBlock extends PillarBlock implements Waterloggable {
+public class HollowLogBlock extends PillarBlock implements CreoBlock, Waterloggable {
     private static final VoxelShape X_SHAPE = VoxelShapes.union(Block.createCuboidShape(0d, 0d, 0d, 16d, 16d, 3d), Block.createCuboidShape(0d, 13d, 0d, 16d, 16d, 16d), Block.createCuboidShape(0d, 0d, 13d, 16d, 16d, 16d), Block.createCuboidShape(0d, 0d, 0d, 16d, 3d, 16d));
     private static final VoxelShape Y_SHAPE = VoxelShapes.union(Block.createCuboidShape(0d, 0d, 0d, 16d, 16d, 3d), Block.createCuboidShape(0d, 0d, 0d, 3d, 16d, 16d), Block.createCuboidShape(0d, 0d, 13d, 16d, 16d, 16d), Block.createCuboidShape(13d, 0d, 0d, 16d, 16d, 16d));
     private static final VoxelShape Z_SHAPE = VoxelShapes.union(Block.createCuboidShape(13d, 0d, 0d, 16d, 16d, 16d), Block.createCuboidShape(0d, 0d, 0d, 3d, 16d, 16d), Block.createCuboidShape(0d, 13d, 0d, 16d, 16d, 16d), Block.createCuboidShape(0d, 0d, 0d, 16d, 3d, 16d));
@@ -67,5 +71,26 @@ public class HollowLogBlock extends PillarBlock implements Waterloggable {
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
         return getDefaultState().with(AXIS, ctx.getSide().getAxis()).with(WATERLOGGED, ctx.getWorld().getFluidState(ctx.getBlockPos()).isOf(Fluids.WATER));
+    }
+
+    @Override
+    public boolean canEntityCollideAdjacent(Entity entity, BlockState state, BlockPos pos) {
+        if (entity.isInPose(EntityPose.SWIMMING) || !entity.isSprinting() || entity.hasPassengers())
+            return false;
+        BlockPos difference = pos.subtract(entity.getBlockPos());
+        if (difference.getY() > .5d || difference.getY() < -.5d || difference.equals(BlockPos.ORIGIN))
+            return false;
+        Vec3i facingVec = entity.getHorizontalFacing().getVector();
+        return switch (state.get(AXIS)) {
+            case X -> difference.getX() != 0d && difference.getX() == facingVec.getX() && difference.getZ() == 0d;
+            case Z -> difference.getZ() != 0d && difference.getZ() == facingVec.getZ() && difference.getX() == 0d;
+            case Y -> false;
+        };
+    }
+
+    @Override
+    public void onAdjacentEntityCollision(Entity entity, BlockState state, BlockPos pos) {
+        entity.setSwimming(true);
+        entity.setPose(EntityPose.SWIMMING);
     }
 }

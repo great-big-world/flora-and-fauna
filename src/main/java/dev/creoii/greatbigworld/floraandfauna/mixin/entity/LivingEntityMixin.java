7e10b2b18cb376898d0c6f6c68cb2cea32cf8fb6
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-09-06 15:30:31 -0400

    Fix climbing inside of single hollow logs

--- src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/entity/LivingEntityMixin.java
+++ src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/entity/LivingEntityMixin.java
@@ -4,9 +4,11 @@ import com.llamalad7.mixinextras.sugar.Local;
 import dev.creoii.greatbigworld.floraandfauna.block.HollowLogBlock;
 import dev.creoii.greatbigworld.floraandfauna.util.FloraAndFaunaTags;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import org.apache.commons.lang3.mutable.MutableBoolean;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,12 +18,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Optional;
 
 @Mixin(LivingEntity.class)
-public class LivingEntityMixin {
+public abstract class LivingEntityMixin extends Entity {
     @Shadow private Optional<BlockPos> climbingPos;
+    @Shadow public abstract EntityDimensions getDimensions(EntityPose pose);
+
+    public LivingEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
 
     @Inject(method = "isClimbing", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isIn(Lnet/minecraft/registry/tag/TagKey;)Z"), cancellable = true)
     private void gbw$livingClimbHollowLogs(CallbackInfoReturnable<Boolean> cir, @Local BlockPos blockPos, @Local BlockState blockState) {
-        if (blockState.isIn(FloraAndFaunaTags.HOLLOW_LOGS) && blockState.get(HollowLogBlock.AXIS) == Direction.Axis.Y) {
+        MutableBoolean canClimbHollowLog = new MutableBoolean(true);
+        BlockPos.stream(getDimensions(EntityPose.STANDING).getBoxAt(blockPos.toCenterPos()).contract(.5d)).forEach(pos -> {
+            BlockState state = getWorld().getBlockState(pos);
+            if (!state.isIn(FloraAndFaunaTags.HOLLOW_LOGS) || state.get(HollowLogBlock.AXIS) != Direction.Axis.Y) {
+                canClimbHollowLog.setFalse();
+            }
+        });
+
+        if (canClimbHollowLog.booleanValue()) {
             climbingPos = Optional.of(blockPos);
             cir.setReturnValue(true);
         }

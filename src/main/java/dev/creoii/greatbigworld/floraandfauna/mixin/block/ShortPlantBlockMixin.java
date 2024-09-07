7e4dca5f3e01c7dcaa2089cab685c24ccd8d2311
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-09-06 21:04:40 -0400

    Vanilla flower block loot tables

--- src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/block/ShortPlantBlockMixin.java
+++ src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/block/ShortPlantBlockMixin.java
@@ -25,7 +25,6 @@ import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 

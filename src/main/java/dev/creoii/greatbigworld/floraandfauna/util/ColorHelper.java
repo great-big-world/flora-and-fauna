2f61558f7b019a836205ee76f6ede4277d238eac
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2024-09-20 21:30:32 -0400

    Add amethyst geodes back to Deep Dark

--- src/main/java/dev/creoii/greatbigworld/floraandfauna/util/ColorHelper.java
+++ src/main/java/dev/creoii/greatbigworld/floraandfauna/util/ColorHelper.java
@@ -5,6 +5,10 @@ public final class ColorHelper {
         return (Math.min(red(color) + r, 255) << 16) | (Math.min(green(color) + g, 255) << 8) | Math.min(blue(color) + b, 255);
     }
 
+    public static int add(int color, int r, int g, int b, int a) {
+        return (Math.min(alpha(color) + a, 255) << 24) | (Math.min(red(color) + r, 255) << 16) | (Math.min(green(color) + g, 255) << 8) | Math.min(blue(color) + b, 255);
+    }
+
     public static int interpolate(double delta, int color1, int color2) {
         delta = Math.max(Math.min(delta, 1f), 0f);
 
@@ -13,6 +17,10 @@ public final class ColorHelper {
         int blue = (int) (blue(color1) + ((blue(color2) - blue(color1)) * delta));
 
         return Math.max(Math.min(red, 255), 0) << 16 | Math.max(Math.min(green, 255), 0) << 8 | Math.max(Math.min(blue, 255), 0);
+    }
+
+    public static int alpha(int color) {
+        return color >> 24 & 0xff;
     }
 
     public static int red(int color) {

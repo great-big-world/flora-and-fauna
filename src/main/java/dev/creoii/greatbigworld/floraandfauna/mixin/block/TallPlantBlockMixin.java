c38967e4f136222fea32576291d65d95df1f95a4
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-02-16 14:01:57 -0500

    Update TallPlantBlockMixin.java

--- src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/block/TallPlantBlockMixin.java
+++ src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/block/TallPlantBlockMixin.java
@@ -92,19 +92,12 @@ public abstract class TallPlantBlockMixin extends PlantBlock implements OverlayS
     @Override
     public boolean canReplace(BlockState state, ItemPlacementContext context) {
         int i = state.get(SnowyHelper.SNOW_LAYERS);
-        if (i > 0) {
-            BlockState down = context.getWorld().getBlockState(context.getBlockPos().down());
-            if (state.get(HALF) == DoubleBlockHalf.LOWER) {
-                if (context.getStack().isOf(Items.SNOW) && i < 8) {
-                    return true;
-                } else return i == 1;
-            } else if (down.isOf(this) && down.get(SnowyHelper.SNOW_LAYERS) == 8) {
-                if (context.getStack().isOf(Items.SNOW) && i < 8) {
-                    return true;
-                } else return i == 1;
-            }
-        }
-        return super.canReplace(state, context);
+        BlockState down = context.getWorld().getBlockState(context.getBlockPos().down());
+        if (state.get(HALF) == DoubleBlockHalf.LOWER) {
+            return context.getStack().isOf(Items.SNOW) && i < 8;
+        } else if (down.isOf(this) && down.get(SnowyHelper.SNOW_LAYERS) == 8) {
+            return context.getStack().isOf(Items.SNOW) && i < 8;
+        } else return super.canReplace(state, context);
     }
 
     @Override

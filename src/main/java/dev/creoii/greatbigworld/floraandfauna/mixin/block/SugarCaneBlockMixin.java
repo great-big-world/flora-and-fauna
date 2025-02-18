d86932a4a3144839e913b899438f713025ca9628
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-02-17 23:52:08 -0500

    Sugar cane can be placed next to Ice

---
+++ src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/block/SugarCaneBlockMixin.java
@@ -0,0 +1,17 @@
+package dev.creoii.greatbigworld.floraandfauna.mixin.block;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.SugarCaneBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(SugarCaneBlock.class)
+public class SugarCaneBlockMixin {
+    @ModifyExpressionValue(method = "canPlaceAt", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isOf(Lnet/minecraft/block/Block;)Z", ordinal = 1))
+    private boolean gbw$allowSugarCaneNextToIce(boolean original, @Local(ordinal = 2) BlockState blockState2) {
+        return original || blockState2.isOf(Blocks.ICE);
+    }
+}

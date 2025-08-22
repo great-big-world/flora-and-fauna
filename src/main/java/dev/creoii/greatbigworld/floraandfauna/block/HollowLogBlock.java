7bf0f63d0c06215ef392a50b762eb62ef4190d52
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-08-22 02:56:18 -0400

    Fix jitter when entering hollow logs in survival

--- src/main/java/dev/creoii/greatbigworld/floraandfauna/block/HollowLogBlock.java
+++ src/main/java/dev/creoii/greatbigworld/floraandfauna/block/HollowLogBlock.java
@@ -4,21 +4,19 @@ import dev.creoii.greatbigworld.block.AdjacentCollision;
 import net.minecraft.block.*;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityPose;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.math.Vec3i;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.TeleportTarget;
 
 public class HollowLogBlock extends PillarBlock implements Waterloggable, AdjacentCollision {
     private static final VoxelShape X_SHAPE = VoxelShapes.union(Block.createCuboidShape(0d, 0d, 0d, 16d, 16d, 3d), Block.createCuboidShape(0d, 13d, 0d, 16d, 16d, 16d), Block.createCuboidShape(0d, 0d, 13d, 16d, 16d, 16d), Block.createCuboidShape(0d, 0d, 0d, 16d, 3d, 16d));
@@ -74,26 +72,28 @@ public class HollowLogBlock extends PillarBlock implements Waterloggable, Adjace
 
     @Override
     public boolean canEntityCollideAdjacent(Entity entity, BlockState state, BlockPos pos) {
-        if (entity.getWorld().isClient || !entity.isSprinting() || entity.hasPassengers())
+        if (!entity.isSprinting() || entity.hasPassengers() || entity.hasVehicle() || entity.isCrawling())
             return false;
         BlockPos difference = pos.subtract(entity.getBlockPos());
-        if (difference.getY() > .5d || difference.getY() < -.5d || difference.equals(BlockPos.ORIGIN))
+        if (Math.abs(difference.getY()) > .15d || difference.equals(BlockPos.ORIGIN))
             return false;
         return switch (state.get(AXIS)) {
+            case Y -> false;
             case X -> difference.getX() != 0d && difference.getZ() == 0d && difference.getX() == entity.getHorizontalFacing().getOffsetX();
             case Z -> difference.getZ() != 0d && difference.getX() == 0d && difference.getZ() == entity.getHorizontalFacing().getOffsetZ();
-            case Y -> false;
         };
     }
 
     @Override
     public void onAdjacentEntityCollision(Entity entity, BlockState state, BlockPos pos) {
-        entity.setSwimming(true);
-        entity.setPose(EntityPose.SWIMMING);
-        if (!entity.getWorld().isClient) {
+        if (entity instanceof LivingEntity living) {
+            living.setPose(EntityPose.SWIMMING);
+            living.setSwimming(true);
+
             Vec3d targetPos = pos.toBottomCenterPos();
             Vec3d direction = entity.getPos().subtract(targetPos).normalize().multiply(.5d);
-            entity.teleportTo(new TeleportTarget((ServerWorld) entity.getWorld(), targetPos.add(direction.x, .2d, direction.z), entity.getVelocity(), entity.getYaw(), entity.getPitch(), entity1 -> {}));
+            Vec3d destPos = targetPos.add(direction.x, .2d, direction.z);
+            living.setPos(destPos.x, destPos.y, destPos.z);
         }
     }
 }

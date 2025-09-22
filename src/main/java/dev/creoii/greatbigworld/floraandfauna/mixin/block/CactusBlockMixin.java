bf22d9bb92e482efdc23fa04fe62ef515a48d901
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-09-22 02:08:16 -0400

    Fix cactus

--- src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/block/CactusBlockMixin.java
+++ src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/block/CactusBlockMixin.java
@@ -1,21 +1,17 @@
 package dev.creoii.greatbigworld.floraandfauna.mixin.block;
 
-import net.minecraft.block.BlockState;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.CactusBlock;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CactusBlock.class)
 public class CactusBlockMixin {
-    @Inject(method = "randomTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/random/Random;nextDouble()D"), cancellable = true)
-    private void gbw$dontGrowCactusFlowersInOverworld(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
-        if (world.getRegistryKey() == ServerWorld.OVERWORLD) {
-            ci.cancel();
-        }
+    @ModifyExpressionValue(method = "randomTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/CactusBlock;canPlaceAt(Lnet/minecraft/block/BlockState;Lnet/minecraft/world/WorldView;Lnet/minecraft/util/math/BlockPos;)Z"))
+    private boolean gbw$dontGrowCactusFlowersInOverworld(boolean original, @Local(argsOnly = true) ServerWorld world) {
+        return world.getRegistryKey() != World.OVERWORLD;
     }
 }

603fa1709bc1e0b089f13dc06159ba91477a5732
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-11-28 02:07:48 -0500

    Fix Snow in Overworld Winters with ParticleRain
    Remove checks for Snow for precipitation? Why were they there

--- src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/client/compat/particlerain/VersionUtilMixin.java
+++ src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/client/compat/particlerain/VersionUtilMixin.java
@@ -7,10 +7,8 @@ import dev.creoii.greatbigworld.floraandfauna.client.FloraAndFaunaClient;
 import dev.creoii.greatbigworld.floraandfauna.season.Season;
 import dev.creoii.greatbigworld.floraandfauna.season.SeasonManager;
 import dev.creoii.greatbigworld.floraandfauna.util.FloraAndFaunaTags;
-import net.minecraft.block.Blocks;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.LightType;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,14 +19,14 @@ import pigcart.particlerain.VersionUtil;
 public class VersionUtilMixin {
     @WrapOperation(method = "getPrecipitationAt", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/biome/Biome;getPrecipitation(Lnet/minecraft/util/math/BlockPos;I)Lnet/minecraft/world/biome/Biome$Precipitation;"))
     private static Biome.Precipitation gbw$modifyCanSetSnowForWinter(Biome instance, BlockPos pos, int seaLevel, Operation<Biome.Precipitation> original, @Local(argsOnly = true, name = "arg0") World level) {
-        if (level.isClient() && (FloraAndFaunaClient.getCurrentSeason() == Season.WINTER && !level.getBiome(pos).isIn(FloraAndFaunaTags.NOT_AFFECTED_BY_WINTER) && Blocks.SNOW.getDefaultState().canPlaceAt(level, pos) && level.isInHeightLimit(pos.getY()) && level.getLightLevel(LightType.BLOCK, pos) < 10))
+        if (level.isClient() && FloraAndFaunaClient.getCurrentSeason() == Season.WINTER && level.getDimensionEntry().isIn(FloraAndFaunaTags.AFFECTED_BY_SEASONS) && !level.getBiome(pos).isIn(FloraAndFaunaTags.NOT_AFFECTED_BY_WINTER))
             return Biome.Precipitation.SNOW;
         else {
             if (level instanceof ServerWorld serverWorld) {
                 SeasonManager seasonManager = SeasonManager.getInstance(serverWorld.getServer());
                 if (seasonManager == null || !serverWorld.getDimensionEntry().isIn(FloraAndFaunaTags.AFFECTED_BY_SEASONS))
                     return original.call(instance, pos, seaLevel);
-                return (seasonManager.getCurrentSeason() == Season.WINTER && !level.getBiome(pos).isIn(FloraAndFaunaTags.NOT_AFFECTED_BY_WINTER) && Blocks.SNOW.getDefaultState().canPlaceAt(level, pos) && level.isInHeightLimit(pos.getY()) && level.getLightLevel(LightType.BLOCK, pos) < 10) ? Biome.Precipitation.SNOW : original.call(instance, pos, seaLevel);
+                return seasonManager.getCurrentSeason() == Season.WINTER && !level.getBiome(pos).isIn(FloraAndFaunaTags.NOT_AFFECTED_BY_WINTER) ? Biome.Precipitation.SNOW : original.call(instance, pos, seaLevel);
             } else return original.call(instance, pos, seaLevel);
         }
     }
--- src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/client/compat/particlerain/WeatherParticleManagerMixin.java
+++ src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/client/compat/particlerain/WeatherParticleManagerMixin.java
@@ -20,31 +20,31 @@ import pigcart.particlerain.config.ConfigData;
 public class WeatherParticleManagerMixin {
     @WrapWithCondition(method = "tickBlockFX", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;addParticleClient(Lnet/minecraft/particle/ParticleEffect;DDDDDD)V"))
     private static boolean gbw$overrideParticleRainBlockFX(ClientWorld instance, ParticleEffect parameters, double x, double y, double z, double velocityX, double velocityY, double velocityZ, @Local(name = "opts") ConfigData.ParticleData opts, @Local(name = "biome") RegistryEntry<Biome> biome) {
-        return "snow".equals(opts.id) == (FloraAndFaunaClient.getCurrentSeason() == Season.WINTER && !biome.isIn(FloraAndFaunaTags.NOT_AFFECTED_BY_WINTER));
+        return "snow".equals(opts.id) == (FloraAndFaunaClient.getCurrentSeason() == Season.WINTER && instance.getDimensionEntry().isIn(FloraAndFaunaTags.AFFECTED_BY_SEASONS) && !biome.isIn(FloraAndFaunaTags.NOT_AFFECTED_BY_WINTER));
     }
 
     @WrapWithCondition(method = "tickBlockFX", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/ParticleManager;addParticle(Lnet/minecraft/client/particle/Particle;)V"))
-    private static boolean gbw$overrideParticleRainBlockFX(ParticleManager instance, Particle particle, @Local(name = "opts") ConfigData.ParticleData opts, @Local(name = "biome") RegistryEntry<Biome> biome) {
-        return "snow".equals(opts.id) == (FloraAndFaunaClient.getCurrentSeason() == Season.WINTER && !biome.isIn(FloraAndFaunaTags.NOT_AFFECTED_BY_WINTER));
+    private static boolean gbw$overrideParticleRainBlockFX(ParticleManager instance, Particle particle, @Local(name = "opts") ConfigData.ParticleData opts, @Local(name = "biome") RegistryEntry<Biome> biome, @Local(name = "level") ClientWorld level) {
+        return "snow".equals(opts.id) == (FloraAndFaunaClient.getCurrentSeason() == Season.WINTER && level.getDimensionEntry().isIn(FloraAndFaunaTags.AFFECTED_BY_SEASONS) && !biome.isIn(FloraAndFaunaTags.NOT_AFFECTED_BY_WINTER));
     }
 
     @WrapWithCondition(method = "tickSkyFX", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;addParticleClient(Lnet/minecraft/particle/ParticleEffect;DDDDDD)V"))
     private static boolean gbw$overrideParticleRainSkyFX(ClientWorld instance, ParticleEffect parameters, double x, double y, double z, double velocityX, double velocityY, double velocityZ, @Local(name = "biome") RegistryEntry<Biome> biome, @Local(name = "data") ConfigData.ParticleData data) {
-        return "snow".equals(data.id) == (FloraAndFaunaClient.getCurrentSeason() == Season.WINTER && !biome.isIn(FloraAndFaunaTags.NOT_AFFECTED_BY_WINTER));
+        return "snow".equals(data.id) == (FloraAndFaunaClient.getCurrentSeason() == Season.WINTER && instance.getDimensionEntry().isIn(FloraAndFaunaTags.AFFECTED_BY_SEASONS) && !biome.isIn(FloraAndFaunaTags.NOT_AFFECTED_BY_WINTER));
     }
 
     @WrapWithCondition(method = "tickSkyFX", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/ParticleManager;addParticle(Lnet/minecraft/client/particle/Particle;)V"))
-    private static boolean gbw$overrideParticleRainSkyFX(ParticleManager instance, Particle particle, @Local(name = "biome") RegistryEntry<Biome> biome, @Local(name = "data") ConfigData.ParticleData data) {
-        return "snow".equals(data.id) == (FloraAndFaunaClient.getCurrentSeason() == Season.WINTER && !biome.isIn(FloraAndFaunaTags.NOT_AFFECTED_BY_WINTER));
+    private static boolean gbw$overrideParticleRainSkyFX(ParticleManager instance, Particle particle, @Local(name = "biome") RegistryEntry<Biome> biome, @Local(name = "data") ConfigData.ParticleData data, @Local(argsOnly = true, name = "arg0") ClientWorld level) {
+        return "snow".equals(data.id) == (FloraAndFaunaClient.getCurrentSeason() == Season.WINTER && level.getDimensionEntry().isIn(FloraAndFaunaTags.AFFECTED_BY_SEASONS) && !biome.isIn(FloraAndFaunaTags.NOT_AFFECTED_BY_WINTER));
     }
 
     @WrapWithCondition(method = "tickSurfaceFX", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;addParticleClient(Lnet/minecraft/particle/ParticleEffect;DDDDDD)V"))
     private static boolean gbw$overrideParticleRainSurfaceFX(ClientWorld instance, ParticleEffect parameters, double x, double y, double z, double velocityX, double velocityY, double velocityZ, @Local(name = "biome") RegistryEntry<Biome> biome, @Local(name = "data") ConfigData.ParticleData data) {
-        return "snow".equals(data.id) == (FloraAndFaunaClient.getCurrentSeason() == Season.WINTER && !biome.isIn(FloraAndFaunaTags.NOT_AFFECTED_BY_WINTER));
+        return "snow".equals(data.id) == (FloraAndFaunaClient.getCurrentSeason() == Season.WINTER && instance.getDimensionEntry().isIn(FloraAndFaunaTags.AFFECTED_BY_SEASONS) && !biome.isIn(FloraAndFaunaTags.NOT_AFFECTED_BY_WINTER));
     }
 
     @WrapWithCondition(method = "tickSurfaceFX", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/ParticleManager;addParticle(Lnet/minecraft/client/particle/Particle;)V"))
-    private static boolean gbw$overrideParticleRainSurfaceFX(ParticleManager instance, Particle particle, @Local(name = "biome") RegistryEntry<Biome> biome, @Local(name = "data") ConfigData.ParticleData data) {
-        return "snow".equals(data.id) == (FloraAndFaunaClient.getCurrentSeason() == Season.WINTER && !biome.isIn(FloraAndFaunaTags.NOT_AFFECTED_BY_WINTER));
+    private static boolean gbw$overrideParticleRainSurfaceFX(ParticleManager instance, Particle particle, @Local(name = "biome") RegistryEntry<Biome> biome, @Local(name = "data") ConfigData.ParticleData data, @Local(argsOnly = true, name = "arg0") ClientWorld level) {
+        return "snow".equals(data.id) == (FloraAndFaunaClient.getCurrentSeason() == Season.WINTER && level.getDimensionEntry().isIn(FloraAndFaunaTags.AFFECTED_BY_SEASONS) && !biome.isIn(FloraAndFaunaTags.NOT_AFFECTED_BY_WINTER));
     }
 }

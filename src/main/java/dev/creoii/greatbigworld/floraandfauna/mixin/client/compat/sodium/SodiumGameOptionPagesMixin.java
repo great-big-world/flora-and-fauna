ba7b676d577356e9749576a41c8340043fef6342
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2026-02-24 00:03:01 -0500

    Fix mixin error
    Update mixins.json

--- src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/client/compat/sodium/SodiumGameOptionPagesMixin.java
+++ src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/client/compat/sodium/SodiumGameOptionPagesMixin.java
@@ -2,7 +2,6 @@ package dev.creoii.greatbigworld.floraandfauna.mixin.client.compat.sodium;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import dev.creoii.greatbigworld.GreatBigWorld;
 import dev.creoii.greatbigworld.floraandfauna.season.TransitionQuality;
 import dev.creoii.greatbigworld.floraandfauna.util.ExtendedGameOptions;
 import net.caffeinemc.mods.sodium.api.config.StorageEventHandler;
@@ -32,7 +31,7 @@ public class SodiumGameOptionPagesMixin {
     private Options vanillaOpts;
 
     @ModifyExpressionValue(method = "buildQualityPage", at = @At(value = "INVOKE", target = "Lnet/caffeinemc/mods/sodium/api/config/structure/OptionGroupBuilder;addOption(Lnet/caffeinemc/mods/sodium/api/config/structure/OptionBuilder;)Lnet/caffeinemc/mods/sodium/api/config/structure/OptionGroupBuilder;", ordinal = 1), remap = false)
-    private OptionGroupBuilder gbw$addSodiumSeasonTransitionQualityOption(OptionGroupBuilder original, @Local(argsOnly = true, name = "arg1") ConfigBuilder builder) {
+    private OptionGroupBuilder gbw$addSodiumSeasonTransitionQualityOption(OptionGroupBuilder original, @Local(argsOnly = true) ConfigBuilder builder) {
         return original.addOption(builder.createEnumOption(Identifier.fromNamespaceAndPath("sodium", "season_transition_quality"), TransitionQuality.class).setName(Component.translatable("options.seasonTransitionQuality")).setTooltip(Component.empty()).setElementNameProvider(EnumOptionBuilder.nameProviderFrom(TransitionQuality.NAMES)).setBinding(quality -> {
             ClientPlayNetworking.send(new TransitionQuality.SyncTransitionQuality((byte) quality.getQuality()));
         }, () -> ((ExtendedGameOptions) vanillaOpts).gbw$getSeasonTransitionQuality().get()).setStorageHandler(vanillaStorage).setDefaultValue(TransitionQuality.NORMAL).setImpact(OptionImpact.VARIES).setFlags(OptionFlag.REQUIRES_RENDERER_UPDATE));

5b2bc1b8f8688bb18dc39d65e937f24ba64a40d7
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2026-04-30 21:27:14 -0400

    Improvements to season tinting
    Frozen rivers are no longer affected by Autumn
    Snowy Grass Blocks now use the snow texture on the top

--- src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/client/BiomeColorsMixin.java
+++ src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/client/BiomeColorsMixin.java
@@ -3,9 +3,12 @@ package dev.creoii.greatbigworld.floraandfauna.mixin.client;
 import dev.creoii.greatbigworld.floraandfauna.client.FloraAndFaunaClient;
 import dev.creoii.greatbigworld.floraandfauna.season.Season;
 import dev.creoii.greatbigworld.floraandfauna.util.FloraAndFaunaTags;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.BiomeColors;
+import net.minecraft.client.renderer.chunk.RenderSectionRegion;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.dimension.BuiltinDimensionTypes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,22 +18,57 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class BiomeColorsMixin {
     @Inject(method = "getAverageFoliageColor", at = @At("RETURN"), cancellable = true)
     private static void gbw$modifyBiomeFoliageColor(BlockAndTintGetter world, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
-        if (FloraAndFaunaClient.getCurrentSeason() != null && !world.getBlockState(pos).is(FloraAndFaunaTags.IGNORE_SEASON_COLOR)) {
-            if (FloraAndFaunaClient.getTransitionContext() != null) {
-                cir.setReturnValue(FloraAndFaunaClient.getTransitionContext().getSeasonFoliageColor(world, pos, cir.getReturnValue()));
-            } else {
-                cir.setReturnValue(FloraAndFaunaClient.getCurrentSeason().getFoliageColorChange().apply(new Season.Context(world, pos, cir.getReturnValue())));
+        if (!world.getBlockState(pos).is(FloraAndFaunaTags.IGNORE_SEASON_COLOR)) {
+            if (world instanceof RenderSectionRegion region && region.level.dimensionTypeRegistration().is(BuiltinDimensionTypes.OVERWORLD)) {
+                System.out.println("BiomeColorsMixin Foliage");
+                Season.Context context = new Season.Context(world, pos, cir.getReturnValue());
+                cir.setReturnValue(Season.SPRING.getGrassColorChange().apply(context));
+                return;
+            }
+            if (FloraAndFaunaClient.getCurrentSeason() != null) {
+                if (FloraAndFaunaClient.getTransitionContext() != null) {
+                    cir.setReturnValue(FloraAndFaunaClient.getTransitionContext().getSeasonFoliageColor(world, pos, cir.getReturnValue()));
+                } else {
+                    cir.setReturnValue(FloraAndFaunaClient.getCurrentSeason().getFoliageColorChange().apply(new Season.Context(world, pos, cir.getReturnValue())));
+                }
             }
         }
     }
 
     @Inject(method = "getAverageGrassColor", at = @At("RETURN"), cancellable = true)
     private static void gbw$modifyBiomeGrassColor(BlockAndTintGetter world, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
-        if (FloraAndFaunaClient.getCurrentSeason() != null && !world.getBlockState(pos).is(FloraAndFaunaTags.IGNORE_SEASON_COLOR)) {
-            if (FloraAndFaunaClient.getTransitionContext() != null) {
-                cir.setReturnValue(FloraAndFaunaClient.getTransitionContext().getSeasonGrassColor(world, pos, cir.getReturnValue()));
-            } else {
-                cir.setReturnValue(FloraAndFaunaClient.getCurrentSeason().getGrassColorChange().apply(new Season.Context(world, pos, cir.getReturnValue())));
+        if (!world.getBlockState(pos).is(FloraAndFaunaTags.IGNORE_SEASON_COLOR)) {
+            if (world instanceof RenderSectionRegion region && region.level.dimensionTypeRegistration().is(BuiltinDimensionTypes.OVERWORLD)) {
+                System.out.println("BiomeColorsMixin Grass");
+                Season.Context context = new Season.Context(world, pos, cir.getReturnValue());
+                cir.setReturnValue(Season.SPRING.getGrassColorChange().apply(context));
+                return;
+            }
+            if (FloraAndFaunaClient.getCurrentSeason() != null) {
+                if (FloraAndFaunaClient.getTransitionContext() != null) {
+                    cir.setReturnValue(FloraAndFaunaClient.getTransitionContext().getSeasonGrassColor(world, pos, cir.getReturnValue()));
+                } else {
+                    cir.setReturnValue(FloraAndFaunaClient.getCurrentSeason().getGrassColorChange().apply(new Season.Context(world, pos, cir.getReturnValue())));
+                }
+            }
+        }
+    }
+
+    @Inject(method = "getAverageDryFoliageColor(Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/core/BlockPos;)I", at = @At("RETURN"), cancellable = true)
+    private static void gbw$modifyBiomeDryFoliageColor(BlockAndTintGetter world, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
+        if (!world.getBlockState(pos).is(FloraAndFaunaTags.IGNORE_SEASON_COLOR)) {
+            if (world instanceof RenderSectionRegion region && region.level.dimensionTypeRegistration().is(BuiltinDimensionTypes.OVERWORLD)) {
+                System.out.println("BiomeColorsMixin Dry Foliage");
+                Season.Context context = new Season.Context(world, pos, cir.getReturnValue());
+                cir.setReturnValue(Season.SPRING.getGrassColorChange().apply(context));
+                return;
+            }
+            if (FloraAndFaunaClient.getCurrentSeason() != null) {
+                if (FloraAndFaunaClient.getTransitionContext() != null) {
+                    cir.setReturnValue(FloraAndFaunaClient.getTransitionContext().getSeasonGrassColor(world, pos, cir.getReturnValue()));
+                } else {
+                    cir.setReturnValue(FloraAndFaunaClient.getCurrentSeason().getGrassColorChange().apply(new Season.Context(world, pos, cir.getReturnValue())));
+                }
             }
         }
     }
--- src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/client/BlockColorsMixin.java
+++ src/main/java/dev/creoii/greatbigworld/floraandfauna/mixin/client/BlockColorsMixin.java
@@ -5,12 +5,12 @@ import dev.creoii.greatbigworld.floraandfauna.season.Season;
 import dev.creoii.greatbigworld.floraandfauna.util.FloraAndFaunaTags;
 import net.minecraft.client.color.block.BlockColors;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.renderer.BiomeColors;
+import net.minecraft.client.renderer.chunk.RenderSectionRegion;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockAndTintGetter;
-import net.minecraft.world.level.GrassColor;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.dimension.BuiltinDimensionTypes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -31,57 +31,73 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class BlockColorsMixin {
     @Inject(method = "getColor(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;)I", at = @At(value = "RETURN", ordinal = 0), cancellable = true)
     private void gbw$modifyParticleColor(BlockState state, Level world, BlockPos pos, CallbackInfoReturnable<Integer> cir) {
-        if (FloraAndFaunaClient.getCurrentSeason() != null && !state.is(FloraAndFaunaTags.IGNORE_SEASON_COLOR) && world.dimensionTypeRegistration().is(FloraAndFaunaTags.AFFECTED_BY_SEASONS)) {
-            if (FloraAndFaunaClient.getTransitionContext() != null) {
-                cir.setReturnValue(FloraAndFaunaClient.getTransitionContext().getSeasonGrassColor(world, pos, cir.getReturnValue()));
-            } else cir.setReturnValue(FloraAndFaunaClient.getCurrentSeason().getParticleColorChange().apply(new Season.Context(world, pos, cir.getReturnValue())));
+        if (!state.is(FloraAndFaunaTags.IGNORE_SEASON_COLOR)) {
+            if (FloraAndFaunaClient.getCurrentSeason() != null && world.dimensionTypeRegistration().is(FloraAndFaunaTags.AFFECTED_BY_SEASONS)) {
+                if (FloraAndFaunaClient.getTransitionContext() != null) {
+                    cir.setReturnValue(FloraAndFaunaClient.getTransitionContext().getSeasonGrassColor(world, pos, cir.getReturnValue()));
+                } else
+                    cir.setReturnValue(FloraAndFaunaClient.getCurrentSeason().getParticleColorChange().apply(new Season.Context(world, pos, cir.getReturnValue())));
+            } else if (world.dimensionTypeRegistration().is(BuiltinDimensionTypes.OVERWORLD)) {
+                System.out.println("BlockColorsMixin Particle2");
+                Season.Context context = new Season.Context(world, pos, cir.getReturnValue());
+                cir.setReturnValue(Season.SPRING.getGrassColorChange().apply(context));
+            }
         }
     }
 
     @Inject(method = "getColor(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockAndTintGetter;Lnet/minecraft/core/BlockPos;I)I", at = @At(value = "RETURN"), cancellable = true)
     private void gbw$modifyParticleColor(BlockState state, BlockAndTintGetter world, BlockPos pos, int i, CallbackInfoReturnable<Integer> cir) {
-        if (FloraAndFaunaClient.getCurrentSeason() != null && world instanceof Level level && !state.is(FloraAndFaunaTags.IGNORE_SEASON_COLOR) && level.dimensionTypeRegistration().is(FloraAndFaunaTags.AFFECTED_BY_SEASONS)) {
-            if (FloraAndFaunaClient.getTransitionContext() != null) {
-                cir.setReturnValue(FloraAndFaunaClient.getTransitionContext().getSeasonGrassColor(level, pos, cir.getReturnValue()));
-            } else cir.setReturnValue(FloraAndFaunaClient.getCurrentSeason().getParticleColorChange().apply(new Season.Context(level, pos, cir.getReturnValue())));
+        if (!state.is(FloraAndFaunaTags.IGNORE_SEASON_COLOR)) {
+            if (FloraAndFaunaClient.getCurrentSeason() != null && world instanceof Level level && level.dimensionTypeRegistration().is(FloraAndFaunaTags.AFFECTED_BY_SEASONS)) {
+                if (FloraAndFaunaClient.getTransitionContext() != null) {
+                    cir.setReturnValue(FloraAndFaunaClient.getTransitionContext().getSeasonGrassColor(level, pos, cir.getReturnValue()));
+                } else
+                    cir.setReturnValue(FloraAndFaunaClient.getCurrentSeason().getParticleColorChange().apply(new Season.Context(level, pos, cir.getReturnValue())));
+            } else if (world instanceof RenderSectionRegion region && region.level.dimensionTypeRegistration().is(BuiltinDimensionTypes.OVERWORLD)) {
+                System.out.println("BlockColorsMixin Particle1");
+                Season.Context context = new Season.Context(world, pos, cir.getReturnValue());
+                cir.setReturnValue(Season.SPRING.getGrassColorChange().apply(context));
+            }
         }
-    }
-
-    @Inject(method = "method_1693", at = @At("HEAD"), cancellable = true)
-    private static void gbw$modifySmallPlantColor(BlockState state, BlockAndTintGetter world, BlockPos pos, int i, CallbackInfoReturnable<Integer> cir) {
-        int baseColor = world != null && pos != null ? BiomeColors.getAverageGrassColor(world, pos) : GrassColor.getDefaultColor();
-        if (FloraAndFaunaClient.getCurrentSeason() != null && !state.is(FloraAndFaunaTags.IGNORE_SEASON_COLOR) && world instanceof ClientLevel clientWorld && clientWorld.dimensionTypeRegistration().is(FloraAndFaunaTags.AFFECTED_BY_SEASONS)) {
-            if (FloraAndFaunaClient.getTransitionContext() != null) {
-                cir.setReturnValue(FloraAndFaunaClient.getTransitionContext().getSeasonGrassColor(world, pos, baseColor));
-            } else cir.setReturnValue(FloraAndFaunaClient.getCurrentSeason().getGrassColorChange().apply(new Season.Context(world, pos, baseColor)));
-            return;
-        }
-        cir.setReturnValue(baseColor);
     }
 
     @Inject(method = "method_1695", at = @At("HEAD"), cancellable = true)
     private static void gbw$modifySpruceColor(BlockState state, BlockAndTintGetter world, BlockPos pos, int i, CallbackInfoReturnable<Integer> cir) {
-        if (world instanceof ClientLevel clientWorld && !clientWorld.dimensionTypeRegistration().is(FloraAndFaunaTags.AFFECTED_BY_SEASONS)) {
-            cir.setReturnValue(-10380959);
-            return;
-        }
-        if (FloraAndFaunaClient.getCurrentSeason() != null && !state.is(FloraAndFaunaTags.IGNORE_SEASON_COLOR)) {
-            if (FloraAndFaunaClient.getTransitionContext() != null) {
-                cir.setReturnValue(FloraAndFaunaClient.getTransitionContext().getSeasonFoliageColor(world, pos, -10380959));
-            } else cir.setReturnValue(FloraAndFaunaClient.getCurrentSeason().getFoliageColorChange().apply(new Season.Context(world, pos, -10380959)));
+        if (!state.is(FloraAndFaunaTags.IGNORE_SEASON_COLOR)) {
+            if (world instanceof ClientLevel clientWorld && !clientWorld.dimensionTypeRegistration().is(FloraAndFaunaTags.AFFECTED_BY_SEASONS)) {
+                cir.setReturnValue(-10380959);
+                return;
+            } else if (world instanceof RenderSectionRegion region && region.level.dimensionTypeRegistration().is(BuiltinDimensionTypes.OVERWORLD)) {
+                System.out.println("BlockColorsMixin Spruce");
+                Season.Context context = new Season.Context(world, pos, -10380959);
+                cir.setReturnValue(Season.SPRING.getGrassColorChange().apply(context));
+            }
+            if (FloraAndFaunaClient.getCurrentSeason() != null) {
+                if (FloraAndFaunaClient.getTransitionContext() != null) {
+                    cir.setReturnValue(FloraAndFaunaClient.getTransitionContext().getSeasonFoliageColor(world, pos, -10380959));
+                } else
+                    cir.setReturnValue(FloraAndFaunaClient.getCurrentSeason().getFoliageColorChange().apply(new Season.Context(world, pos, -10380959)));
+            }
         }
     }
 
     @Inject(method = "method_1687", at = @At("HEAD"), cancellable = true)
     private static void gbw$modifyBirchColor(BlockState state, BlockAndTintGetter world, BlockPos pos, int i, CallbackInfoReturnable<Integer> cir) {
-        if (world instanceof ClientLevel clientWorld && !clientWorld.dimensionTypeRegistration().is(FloraAndFaunaTags.AFFECTED_BY_SEASONS)) {
-            cir.setReturnValue(-8345771);
-            return;
-        }
-        if (FloraAndFaunaClient.getCurrentSeason() != null && !state.is(FloraAndFaunaTags.IGNORE_SEASON_COLOR)) {
-            if (FloraAndFaunaClient.getTransitionContext() != null) {
-                cir.setReturnValue(FloraAndFaunaClient.getTransitionContext().getSeasonFoliageColor(world, pos, -8345771));
-            } else cir.setReturnValue(FloraAndFaunaClient.getCurrentSeason().getFoliageColorChange().apply(new Season.Context(world, pos, -8345771)));
+        if (!state.is(FloraAndFaunaTags.IGNORE_SEASON_COLOR)) {
+            if (world instanceof ClientLevel clientWorld && !clientWorld.dimensionTypeRegistration().is(FloraAndFaunaTags.AFFECTED_BY_SEASONS)) {
+                cir.setReturnValue(-8345771);
+                return;
+            } else if (world instanceof RenderSectionRegion region && region.level.dimensionTypeRegistration().is(BuiltinDimensionTypes.OVERWORLD)) {
+                System.out.println("BlockColorsMixin Birch");
+                Season.Context context = new Season.Context(world, pos, -8345771);
+                cir.setReturnValue(Season.SPRING.getGrassColorChange().apply(context));
+            }
+            if (FloraAndFaunaClient.getCurrentSeason() != null) {
+                if (FloraAndFaunaClient.getTransitionContext() != null) {
+                    cir.setReturnValue(FloraAndFaunaClient.getTransitionContext().getSeasonFoliageColor(world, pos, -8345771));
+                } else
+                    cir.setReturnValue(FloraAndFaunaClient.getCurrentSeason().getFoliageColorChange().apply(new Season.Context(world, pos, -8345771)));
+            }
         }
     }
 }

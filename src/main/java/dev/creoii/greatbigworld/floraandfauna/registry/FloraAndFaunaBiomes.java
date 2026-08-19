471f9c03dc3fccb587e290fc78d9781460de6523
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2026-08-19 14:34:08 -0400

    Improve variant river placement
    Remove swamp rivers
    Fix fallen trees not generating
    Better jungle sky color & fog
    Fix rain in desert rivers

--- src/main/java/dev/creoii/greatbigworld/floraandfauna/registry/FloraAndFaunaBiomes.java
+++ src/main/java/dev/creoii/greatbigworld/floraandfauna/registry/FloraAndFaunaBiomes.java
@@ -1,7 +1,6 @@
 package dev.creoii.greatbigworld.floraandfauna.registry;
 
 import dev.creoii.greatbigworld.GreatBigWorld;
-import dev.creoii.greatbigworld.floraandfauna.FloraAndFauna;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
@@ -11,5 +10,4 @@ public final class FloraAndFaunaBiomes {
     public static final ResourceKey<Biome> COLD_RIVER = ResourceKey.create(Registries.BIOME, Identifier.fromNamespaceAndPath(GreatBigWorld.NAMESPACE, "cold_river"));
     public static final ResourceKey<Biome> DESERT_RIVER = ResourceKey.create(Registries.BIOME, Identifier.fromNamespaceAndPath(GreatBigWorld.NAMESPACE, "desert_river"));
     public static final ResourceKey<Biome> JUNGLE_RIVER = ResourceKey.create(Registries.BIOME, Identifier.fromNamespaceAndPath(GreatBigWorld.NAMESPACE, "jungle_river"));
-    public static final ResourceKey<Biome> SWAMP_RIVER = ResourceKey.create(Registries.BIOME, Identifier.fromNamespaceAndPath(GreatBigWorld.NAMESPACE, "swamp_river"));
 }
